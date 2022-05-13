c50464289753c9d837b97b24120ccc34b5538d56
MoHElsayed000 <94206356+MoHElsayed000@users.noreply.github.com>
2022-05-13 14:39:09 +0200

    Add project files.

---
+++ FINAL NETWORK/UploadAndEditGame/Edit.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FINAL_NETWORK.UploadAndEditGame
+{
+    public partial class Edit : System.Web.UI.Page
+    {
+        //database varbel=>
+        AspDataEntities db = new AspDataEntities();
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1 == null && TextBox2.Text == null && TextBox3 == null)
+            {
+                TextBox1.Text = "Null";
+            }
+            else
+            {
+                var ck = db.GAMEs.SingleOrDefault(x => x.name == TextBox1.Text);
+                if (ck != null)
+                {
+                    if (FileUpload1.HasFile)
+                    {
+                        byte[] imgaq = FileUpload1.FileBytes;
+                        string fileur = FileUpload1.PostedFile.FileName;
+                        string copfile = "urlimg/" + FileUpload1.FileName;
+                        FileUpload1.PostedFile.SaveAs(Server.MapPath("~/urlimg/") + fileur);
+                        ck.price = int.Parse(TextBox2.Text);
+                        ck.description = TextBox3.Text;
+                        ck.type_game = DropDownList1.SelectedValue;
+                        ck.img1 = copfile;
+                    }
+                    else
+                    {
+                        ck.price = int.Parse(TextBox2.Text);
+                        ck.description = TextBox3.Text;
+                        ck.type_game = DropDownList1.SelectedValue;
+                    }
+                }
+                db.SaveChanges();
+                TextBox1.Text = "Done Edit";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var ck = db.GAMEs.SingleOrDefault(x => x.name == TextBox1.Text);
+            if (ck != null)
+            {
+                db.GAMEs.Remove(ck);
+                db.SaveChanges();
+                TextBox1.Text = "Done";
+                TextBox2.Text = "Done";
+                TextBox3.Text = "Done";
+                DropDownList1.Text = "None";
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (TextBox4 != null)
+            {
+                DataList1.CssClass = "dals";
+                SqlConnection con = new SqlConnection(@"Data Source=MOHAMED;Initial Catalog=AspData;Integrated Security=True");
+                SqlDataAdapter sb;
+                sb = new SqlDataAdapter("Select * from GAME where name like '%" + TextBox4.Text + "%'", con);
+                DataTable dt = new DataTable();
+                con.Open();
+                sb.Fill(dt);
+                DataList1.DataSourceID = null;
+                DataList1.DataSource = dt;
+                DataList1.DataBind();
+                con.Close();
+            }
+
+        }
+        protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+
+            DataList1.SelectedIndex = e.Item.ItemIndex;
+            string ser = ((Image)DataList1.SelectedItem.FindControl("Image1")).ImageUrl;
+            var serca = db.GAMEs.SingleOrDefault(x => x.img1 == ser);
+            if (serca != null)
+            {
+                TextBox1.Text = serca.name;
+                TextBox2.Text = serca.price.ToString();
+                TextBox3.Text = serca.description;
+                DropDownList1.Text = serca.type_game.ToString();
+            }
+        }
+
+        protected void Image1_DataBinding(object sender, EventArgs e)
+        {
+
+        }
+        protected void Item_Command(object source, DataListCommandEventArgs e)
+        {
+            DataList1.SelectedIndex = e.Item.ItemIndex;
+            string ser = ((Label)DataList1.SelectedItem.FindControl("Label1")).Text;
+            Button l = new Button();
+            var serca = db.GAMEs.SingleOrDefault(x => x.name == ser);
+            if (serca != null)
+            {
+                TextBox1.Text = serca.name;
+                TextBox2.Text = serca.price.ToString();
+                TextBox3.Text = serca.description;
+                DropDownList1.Text = serca.type_game.ToString();
+            }
+            else
+            {
+                TextBox4.Text = "NO";
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SqlDataSource1_Selected(object sender, SqlDataSourceStatusEventArgs e)
+        {
+
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FINAL NETWORK/UploadAndEditGame/upload_game.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FINAL_NETWORK.UploadAndEditGame
+{
+    public partial class upload_game : System.Web.UI.Page
+    {
+        AspDataEntities db = new AspDataEntities();
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == null && TextBox2.Text == null && TextBox3.Text == null && FileUpload1 == null && DropDownList1.SelectedValue == "None")
+            {
+                Label1.Text = "Error in input";
+                Label1.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                var ch = db.GAMEs.FirstOrDefault(x => x.name == TextBox1.Text);
+                if (ch != null)
+                {
+                    Label1.Text = "Game Already exit";
+                }
+                else
+                {
+
+                    if (FileUpload1.HasFile)
+                    {
+
+                        string fileur = FileUpload1.PostedFile.FileName;
+                        string copfile = "urlimg/" + FileUpload1.FileName;
+                        FileUpload1.PostedFile.SaveAs(Server.MapPath("~/urlimg/") + fileur);
+
+
+                        GAME gam = new GAME
+                        {
+                            name = TextBox1.Text,
+                            price = int.Parse(TextBox2.Text),
+                            description = TextBox3.Text,
+                            time_upload = DateTime.Now,
+                            img1 = copfile,
+                            download_number = 0,
+                            type_game = DropDownList1.SelectedValue
+                        };
+                        db.GAMEs.Add(gam);
+                        db.SaveChanges();
+
+                        Label1.Text = copfile;
+                        Label1.ForeColor = System.Drawing.Color.Green;
+
+
+
+                    }
+                }
+
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
